ffe014a56ee63772c5bb1e8f8c7827e76e2b6394
nirajwadhe <nirajwadhe@gmail.com>
2024-06-11 10:19:18 +0530

    [Niraj] Refactor: REfactor to added a new database added new URL

--- core/setting.py
+++ core/setting.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config=SettingsConfigDict(env_file="core/.env")
+    DB_URL_NOTES:str
     DB_URL:str
     SECRET_KEY:str
     ALGORITHM:str
